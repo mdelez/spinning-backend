8f881f97fd0d398e36b44b8a4c21088a594d1a0c
Mike <mmdelez@gmail.com>
2026-03-09 15:41:10 +0100

    chore: remove console logs

--- src/middleware/requireRole.ts
+++ src/middleware/requireRole.ts
@@ -4,8 +4,6 @@ import { AuthRequest } from "./auth.js";
 export function requireRole(roles: string[]) {
     return (req: AuthRequest, res: Response, next: NextFunction) => {
         const user = req.user;
-        console.log('user id: ', user?.id);
-        console.log('user role: ', user?.role);
 
         if (!user || !roles.includes(user.role)) {
             return res.status(403).json({ error: "Forbidden" });
